35b6c36484f781bd586ee3e636d1766b588e373c
Carlos Ortiz Prieto <carlorpri@gmail.com>
2019-01-14 19:20:44 +0100

    mi archivp
    dsfd

--- java-hello/src/main/java/hello/GreetingController.java
+++ java-hello/src/main/java/hello/GreetingController.java
@@ -14,4 +14,10 @@ public class GreetingController {
         return "greeting";
     }
 
+
+@RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="Universe") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 }
