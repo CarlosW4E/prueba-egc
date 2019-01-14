533a22f98f4864ff655f07447ba0dcc1f4d30971
Carlos Ortiz Prieto <carlorpri@gmail.com>
2019-01-14 19:05:37 +0100

    Pequeño fallo
    Corrido ya

--- java-hello/src/main/java/hello/GreetingController.java
+++ java-hello/src/main/java/hello/GreetingController.java
@@ -14,7 +14,7 @@ public class GreetingController {
         return "greeting";
     }
 @RequestMapping("/bye")
-    public String bye(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    public String bye(@RequestParam(value="name", required=false, defaultValue="Universe") String name, Model model) {
         model.addAttribute("name", name);
         return "bye";
     }
